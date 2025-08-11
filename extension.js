84f3284853d396287442b8257fea45dbf8c5a777
NDilusha <161825980+Dilusha-Ranasingha@users.noreply.github.com>
2025-08-11 16:42:49 +0530

    feat: add axios and fast-xml-parser dependencies; implement RSS feed fetching in activate function
    fetch the web link all xml data

--- extension.js
+++ extension.js
@@ -1,10 +1,14 @@
 const vscode = require('vscode');
+const axios = require('axios');
 
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+	const res = await axios.get("https://blog.webdevsimplified.com/rss.xml")
+	console.log(res.data)
+
 	const disposable = vscode.commands.registerCommand('nd-search-blog.NDSearchBlog', function () {
 		vscode.window.showInformationMessage('Hello World from ND Search Blogs!');
 	});
